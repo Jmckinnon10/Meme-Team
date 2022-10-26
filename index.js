c6d413a48f48310a0151ee4e09bf83ac2a67ba96
John McKinnon <johnmckinnon23@yahoo.com>
2022-10-26 15:10:15 -0700

    Randomizer fixed

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 const API = 'https://api.imgflip.com/get_memes'
+const memeButton = document.querySelector('#random-meme')
+memeButton.textContent = "RANDOMIZE"
+
 
   fetch(API)
     .then(resp => resp.json())
     .then(memeData => {
        const memes = memeData.data.memes
     // console.log(newMemes)
-       randomMeme(memes)
-      //  console.log(memeData.data.memes[0].url)
+      // randomMeme(memes)
+      //  console.log(memeData.data.memes[0].url) 
+     // const newMemes = memes[Math.floor((Math.random()*memes.length))]
+   // makeTheMeme(newMemes)
+      memeButton.addEventListener('click', () => {
+          const newMemes = memes[Math.floor((Math.random()*memes.length))]
+         
+          
+          // const memeButton = document.querySelector('#random-meme')
+          // memeButton.textContent = "RANDOMIZE"
+          const memeImage = document.querySelector('#meme-image')
+          const singleMeme = document.querySelector('#image')
+          
+          singleMeme.src = newMemes.url
+          memeImage.append(singleMeme)
+          memeImage.src = newMemes.url
+          console.log(newMemes)
+
+    })
    
   })
 
-  
-  const memeButton = document.querySelector('#random-meme')
-  function randomMeme(memes) {
-    
-    const newMemes = memes[Math.floor((Math.random()*memes.length))]
-    //console.log(newMemes.url)
-     
-    const memeButton = document.querySelector('#random-meme')
-    memeButton.textContent = "RANDOMIZE"
-    const memeImage = document.querySelector('#meme-image')
-    const singleMeme = document.createElement('img')
-    
-    singleMeme.src = newMemes.url
-    memeImage.append(singleMeme)
-    memeImage.src = newMemes.url
-    //console.log(memeImage.src)
 
-  }
+// function makeTheMeme (newMemes) {
+//   memeButton.addEventListener('click', () => {
+//     //const newMemes = memes[Math.floor((Math.random()*memes.length))]
+   
+    
+//     // const memeButton = document.querySelector('#random-meme')
+//     // memeButton.textContent = "RANDOMIZE"
+//     const memeImage = document.querySelector('#meme-image')
+//     const singleMeme = document.querySelector('#image')
+    
+//     singleMeme.src = newMemes.url
+//     memeImage.append(singleMeme)
+//     memeImage.src = newMemes.url
+//     console.log(newMemes)
+
+// })
+// }
+  
 
 
