189101279131a93f42f815ae936a5d45fbe4b4e9
John McKinnon <johnmckinnon23@yahoo.com>
2022-10-27 09:02:18 -0700

    drag boxes

--- index.js
+++ index.js
@@ -6,48 +6,38 @@ memeButton.textContent = "RANDOMIZE"
   fetch(API)
     .then(resp => resp.json())
     .then(memeData => {
-       const memes = memeData.data.memes
-    // console.log(newMemes)
-      // randomMeme(memes)
-      //  console.log(memeData.data.memes[0].url) 
-     // const newMemes = memes[Math.floor((Math.random()*memes.length))]
-   // makeTheMeme(newMemes)
+      const memes = memeData.data.memes;
       memeButton.addEventListener('click', () => {
-          const newMemes = memes[Math.floor((Math.random()*memes.length))]
-         
-          
-          // const memeButton = document.querySelector('#random-meme')
-          // memeButton.textContent = "RANDOMIZE"
-          const memeImage = document.querySelector('#meme-image')
-          const singleMeme = document.querySelector('#image')
-          
-          singleMeme.src = newMemes.url
-          memeImage.append(singleMeme)
-          memeImage.src = newMemes.url
-          console.log(newMemes)
+          const newMemes = memes[Math.floor((Math.random()*memes.length))];
+          const memeImage = document.querySelector('#meme-image');
+          const singleMeme = document.querySelector('#image');
+          boxes = newMemes.box_count;
+          singleMeme.src = newMemes.url;
+          memeImage.append(singleMeme);
+          memeImage.src = newMemes.url;
+          console.log(newMemes);
+          console.log(boxes);
 
+          if(boxes === 2){
+            
+          }
     })
-   
   })
 
+  function allowDrop(e) {
+    e.preventDefault();
+  }
+  
+  function drag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+  }
+  
+  function drop(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    e.target.appendChild(document.getElementById(data));
+  }
 
-// function makeTheMeme (newMemes) {
-//   memeButton.addEventListener('click', () => {
-//     //const newMemes = memes[Math.floor((Math.random()*memes.length))]
-   
-    
-//     // const memeButton = document.querySelector('#random-meme')
-//     // memeButton.textContent = "RANDOMIZE"
-//     const memeImage = document.querySelector('#meme-image')
-//     const singleMeme = document.querySelector('#image')
-    
-//     singleMeme.src = newMemes.url
-//     memeImage.append(singleMeme)
-//     memeImage.src = newMemes.url
-//     console.log(newMemes)
-
-// })
-// }
   
 
 
