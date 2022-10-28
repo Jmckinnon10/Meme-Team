ecf76972d6f84772cb45695d6296b2d1a8d9dd07
John McKinnon <johnmckinnon23@yahoo.com>
2022-10-28 11:23:25 -0700

    CSS updates

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const LOCALAPI = ' http://localhost:3000/memes'
 const API = 'https://api.imgflip.com/get_memes'
 const memeButton = document.querySelector('#random-meme')
-memeButton.textContent = "RANDOMIZE"
-const inputs = document.querySelectorAll('.box')
 const form = document.querySelector('#meme-content')
-const imageCollection = document.querySelector('#collection')
-const imageCard = document.createElement('div')
-imageCard.id = 'card'
+memeButton.textContent = "RANDOMIZE"
 
   fetch(API)
     .then(resp => resp.json())
     .then(memeData => {
       const memes = memeData.data.memes;
-      console.log(memes)
+     // console.log(memes)
       memeButton.addEventListener('click', () => {
           const newMemes = memes[Math.floor((Math.random()*memes.length))];
           const memeImage = document.querySelector('#meme-image');
           const singleMeme = document.querySelector('#image');
           const name = document.querySelector('h2')
-          const url = document.querySelector('p')
+          const url = document.querySelector('#url')
 
           url.textContent = newMemes.url
           name.textContent = newMemes.name
@@ -26,7 +22,6 @@ imageCard.id = 'card'
           singleMeme.src = newMemes.url;
           memeImage.append(singleMeme);
           memeImage.src = newMemes.url;
-          
     })  
   })
       
@@ -46,29 +41,33 @@ function renderImages(memes) {
   //console.log(memes)
   const localImage = document.createElement('img')
   const memeName = document.createElement('h3')
-  
+  const imageCollection = document.querySelector('#collection')
+  const imageCard = document.createElement('div')
+  imageCard.id = 'card'
   memeName.textContent = memes.name
   localImage.src = memes.image
  //console.log(localImage.src)
   imageCollection.append(imageCard)
   imageCard.append(memeName, localImage)
 
-    imageCard.addEventListener('mouseover', () => {
+  imageCard.addEventListener('mouseover', () => {
       localImage.style.transform = "scale(2.5)";
       localImage.style.transition = "transform 1s ease";
-    imageCard.addEventListener('mouseout', () => {
+    }) 
+  imageCard.addEventListener('mouseout', () => {
       localImage.style.transform = "scale(1)";
       localImage.style.transition =
         "transform 1s ease";
-      localImage.style=""
-  })
-  })
+    })
+
 }
 
 form.addEventListener('submit', e => {
   e.preventDefault()
   const newImage = {
-    image: e.target[0].value
+    name: e.target[0].value,
+    image: e.target[1].value
+
   }
     renderImages(newImage)
 })
